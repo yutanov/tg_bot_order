44cfd46bf5f3f983ec232273a7d49587f5a73fca
yutanov <utanov@gmail.com>
2022-04-19 14:30:16 +0300

    __init__.py

--- __init__.py
+++ __init__.py
@@ -46,7 +46,7 @@ def restart(update, _):
 
 
 if __name__ == '__main__':
-    updater = Updater("5198022488:AAHUNXIY2RtbFB4fiTGUeNeoHredWPU-lxM")
+    updater = Updater("TOKEN")
 
     conv_handler = ConversationHandler(
         entry_points=[
