4b5dbc8812bdd6f4740a8910d72c3ed768ac2985
yutanov <utanov@gmail.com>
2022-04-21 00:38:37 +0300

    fix opt_keyboard.py

--- del_keyboard.py
+++ del_keyboard.py
@@ -2,14 +2,11 @@
 
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import CommandHandler, CallbackQueryHandler, MessageHandler, ConversationHandler, Filters
-from steps import start, help_command, cancel_handler
 from questionnaire import BANK, OPTIONS, DELIVERY, OTHER_DELIVERY, OTHER_OPTIONS, OTHER_BANK
 from questionnaire import LOYALTY
 from questionnaire import bank, other_bank, other_delivery, order_at_desk
 from questionnaire import loyalty, other_options
-#from opt_keyboard import keyboard_conv_opt
 from opt_keyboard import start_options, opt_press, opt_clicked
-#from main import start_handler
 import logging
 
 
@@ -92,8 +89,6 @@ def clicked(click=None, data=None):
 
 def press(update, context):
     data = update.callback_query.data
-    # if data != "next":
-    #    print(data)
     global CLICK
     if context.user_data[f'BUTTON_{data}'] == False:
         context.user_data[f'BUTTON_{data}'] = True
@@ -102,82 +97,3 @@ def press(update, context):
     reply_keyboard = clicked(context.user_data[f'BUTTON_{data}'], data)
     update.callback_query.edit_message_text(text=TEXT_1, reply_markup=reply_keyboard)
     return DELIVERY
-    # else:
-    #    print('next ', data)
-    #save(update, contex)
-    #    try:
-    #        context.user_data[DELIVERY] = update.message.text
-    #        return OPTIONS_START
-    #    except:
-    #        text = ''
-    #        for i in range(1, 5):
-    #            text += f'{DEL_DATA[i]} - ' + str(context.user_data[f'BUTTON_{i}']) + '\n'
-    #        context.user_data[DELIVERY] = text
-    #        print('NEXT NEXT NEXT')
-    #        return OPTIONS_START
-
-
-# def save(update, context):
-#    print('HERE')
-#    try:
-#        context.user_data[DELIVERY] = update.message.text
-#        return
-#    except:
-#        text = ''
-#        for i in range(1, 5):
-#            text += f'{DEL_DATA[i]} - ' + str(context.user_data[f'BUTTON_{i}']) + '\n'
-#        context.user_data[DELIVERY] = text
-#        return
-
-#keyboard_conv = ConversationHandler(
-#    entry_points=[
-#        CallbackQueryHandler(start_delivery,
-#                             pattern='^' + 'sber' + '$|^' + 'tinkoff' + '$|^' + 'cp' + '$|^' + 'ukassa' + '$'),
-        #CallbackQueryHandler(other_bank, pattern='^' + 'other' + '$'),
-#        MessageHandler(Filters.all, start_delivery, pass_user_data=True),
-#    ],
-#    states={
-#        DELIVERY: [
-#            CallbackQueryHandler(press,
-#                                 pattern='^'+str(1)+'$|^'+str(2)+'$|^'+str(3)+'$|^'+str(4)+'$'),
-            # CallbackQueryHandler(start_delivery,
-            #                     pattern='^' + 'sber' + '$|^' + 'tinkoff' + '$|^' + 'cp' + '$|^' + 'ukassa' + '$'),
-#            CallbackQueryHandler(other_delivery, pattern='^' + 'other' + '$'),
-#            CallbackQueryHandler(bank, pattern='^' + 'back' + '$'),
-#            CallbackQueryHandler(start_options, pattern='^'+'next'+'$'),
-#        ],
-#        BANK: [
-#            CallbackQueryHandler(start_delivery,
-#                                 pattern='^' + 'sber' + '$|^' + 'tinkoff' + '$|^' + 'cp' + '$|^' + 'ukassa' + '$'),
-#            CallbackQueryHandler(other_bank, pattern='^' + 'other' + '$'),
-#            CallbackQueryHandler(order_at_desk, pattern='^' + 'back' + '$'),
-#        ],
-#        OTHER_BANK: [
-#            MessageHandler(Filters.all, start_delivery, pass_user_data=True),
-#            CallbackQueryHandler(bank, pattern='^' + 'back' + '$'),
-#        ],
-#        OTHER_DELIVERY: [
-#            MessageHandler(Filters.all, start_options, pass_user_data=True),
-#            CallbackQueryHandler(start_delivery, pattern='^' + 'back' + '$'),
-#        ],
-#        OPTIONS: [
-#            CallbackQueryHandler(opt_press,
-#                                 pattern='^'+str(1)+'$|^'+str(2)+'$|^'+str(3)+'$|^'+str(4)+'$|^'+str(5)+'$'),
-#            CallbackQueryHandler(other_options, pattern='^' + 'other' + '$'),
-#            CallbackQueryHandler(loyalty, pattern='^'+'next'+'$'),
-#            CallbackQueryHandler(start_delivery, pattern='^' + 'back' + '$'),
-#        ],
-#        OTHER_OPTIONS: [
-#            MessageHandler(Filters.all, loyalty, pass_user_data=True),
-#            CallbackQueryHandler(start_options, pattern='^' + 'back' + '$'),
-#        ],
-#    },
-#    map_to_parent={
-#        LOYALTY: LOYALTY,
-#    },
-#    fallbacks=[
-#        CommandHandler('start', start),
-#        CommandHandler('help', help_command),
-#        CommandHandler('cancel', cancel_handler),
-#    ],
-#)
